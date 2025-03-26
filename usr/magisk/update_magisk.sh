726c5f05a3f319589798be494226d43ece970671
jenmck10 <94679349+jenmck10@users.noreply.github.com>
2025-03-26 17:55:45 +0000

    Update update_magisk.sh

--- usr/magisk/update_magisk.sh
+++ usr/magisk/update_magisk.sh
@@ -38,22 +38,22 @@ then
 		mv -f "$DIR/arm/magiskinit64" "$DIR/magiskinit"
 		: > "$DIR/magisk32.xz"
 		: > "$DIR/magisk64.xz"
-	elif unzip -o "$DIR/magisk.zip" lib/armeabi-v7a/libmagiskinit.so lib/armeabi-v7a/libmagisk32.so lib/armeabi-v7a/libmagisk64.so -d "$DIR"; then
+	elif unzip -o "$DIR/magisk.zip" lib/armeabi-v7a/libmagiskinit.so lib/armeabi-v7a/libmagisk.so lib/armeabi-v7a/libmagisk64.so -d "$DIR"; then
 		mv -f "$DIR/lib/armeabi-v7a/libmagiskinit.so" "$DIR/magiskinit"
-		mv -f "$DIR/lib/armeabi-v7a/libmagisk32.so" "$DIR/magisk32"
+		mv -f "$DIR/lib/armeabi-v7a/libmagisk.so" "$DIR/magisk32"
 		mv -f "$DIR/lib/armeabi-v7a/libmagisk64.so" "$DIR/magisk64"
 		xz --force --check=crc32 "$DIR/magisk32" "$DIR/magisk64"
-	elif unzip -o "$DIR/magisk.zip" lib/arm64-v8a/libmagiskinit.so lib/armeabi-v7a/libmagisk32.so lib/arm64-v8a/libmagisk64.so assets/stub.apk -d "$DIR"; then
+	elif unzip -o "$DIR/magisk.zip" lib/arm64-v8a/libmagiskinit.so lib/armeabi-v7a/libmagisk.so lib/arm64-v8a/libmagisk.so assets/stub.apk -d "$DIR"; then
 		mv -f "$DIR/lib/arm64-v8a/libmagiskinit.so" "$DIR/magiskinit"
-		mv -f "$DIR/lib/armeabi-v7a/libmagisk32.so" "$DIR/magisk32"
-		mv -f "$DIR/lib/arm64-v8a/libmagisk64.so" "$DIR/magisk64"
+		mv -f "$DIR/lib/armeabi-v7a/libmagisk.so" "$DIR/magisk32"
+		mv -f "$DIR/lib/arm64-v8a/libmagisk.so" "$DIR/magisk64"
 		mv -f "$DIR/assets/stub.apk" "$DIR/stub"
 		xz --force --check=crc32 "$DIR/magisk32" "$DIR/magisk64" "$DIR/stub"
 	else
-		unzip -o "$DIR/magisk.zip" lib/arm64-v8a/libmagiskinit.so lib/armeabi-v7a/libmagisk32.so lib/arm64-v8a/libmagisk64.so -d "$DIR"
+		unzip -o "$DIR/magisk.zip" lib/arm64-v8a/libmagiskinit.so lib/armeabi-v7a/libmagisk.so lib/arm64-v8a/libmagisk.so -d "$DIR"
 		mv -f "$DIR/lib/arm64-v8a/libmagiskinit.so" "$DIR/magiskinit"
-		mv -f "$DIR/lib/armeabi-v7a/libmagisk32.so" "$DIR/magisk32"
-		mv -f "$DIR/lib/arm64-v8a/libmagisk64.so" "$DIR/magisk64"
+		mv -f "$DIR/lib/armeabi-v7a/libmagisk.so" "$DIR/magisk32"
+		mv -f "$DIR/lib/arm64-v8a/libmagisk.so" "$DIR/magisk64"
 		xz --force --check=crc32 "$DIR/magisk32" "$DIR/magisk64"
 	fi
 	echo -n "$nver" > "$DIR/magisk_version"
